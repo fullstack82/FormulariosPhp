37d890fb87da53b038f2697bb93622a57b5fc33d
fullstack82 <fullstack8210@gmail.com>
2021-01-31 20:58:58 +0100

    repaso a etiquetas html

--- index.php
+++ index.php
@@ -7,28 +7,66 @@
 </head>
 
 <body>
+    <!--Todos los tipos de campos que hay en html-->
     <h1>Formulario</h1>
     <form action="" method="POST" enctype="multipart/form-data">
         <!-- Multipart permite enviar archivos desde el formulario al servidor -->
         <label for="nombre">Nombre:</label>
         <p> <input type="text" name="nombre" autofocus="autofocus" /></p>
 
-        <label for="apellido:">Apellido:</label>
+        <!--APellidos-->
+        <label for="apellido">Apellido:</label>
         <p> <input type="text" name="apellido" required /></p>
 
-        <label for="boton:">Boton:</label>
+        <!--Crear un boton-->
+        <label for="boton">Boton:</label>
         <p> <input type="button" name="boton" value="Haz click" /></p>
 
-        <label for="sexo:">Sexo:</label>
+
+        <!--Checkbox-->
+        <label for="sexo">Sexo:</label>
         <p> <input type="checkbox" name="sexo" value="hombre" checked="check" />Hombre
             <input type="checkbox" name="sexo" value="mujer" />Mujer
         </p>
 
-        <label for="color:">Color:</label>
-        <p> <input type="color" name="color"  /></p>
+        <!--Saca un cuadro de color-->
+        <label for="color">Color:</label>
+        <p> <input type="color" name="color" /></p>
 
-        <label for="fecha:">Fecha:</label>
+        <!--Meter fecha-->
+        <label for="fecha">Fecha:</label>
         <p> <input type="date" name="fecha" /></p>
+
+        <!--Email-->
+        <label for="email">Email:</label>
+        <p> <input type="email" name="email" /></p>
+
+        <!--Subir multiples archivos-->
+        <label for="archivo">Archivos:</label>
+        <p> <input type="file" name="archivo" multiple="multiple" /></p>
+
+        <!--Campo de numeros (+ o -) -->
+        <label for="numero">Número:</label>
+        <p> <input type="number" name="numero" /></p>
+
+        <!--Password-->
+        <label for="pass">Password:</label>
+        <p> <input type="password" name="pass" /></p>
+
+
+        <!--Radio-button-->
+        <label for="continente">¿De que continente accedes?:</label>
+        <p> <input type="radio" name="continente" value="America del Sur"/>America del Sur</p>
+        <p> <input type="radio" name="continente" value="Europa"/>Europa</p>
+        <p> <input type="radio" name="continente" value="Asia"/>Asia</p>
+        <p> <input type="radio" name="continente" value="Africa"/>Africa</p>
+        <p> <input type="radio" name="continente" value="America del Norte"/>America del Norte</p>
+        <p> <input type="radio" name="continente" value="Antartida"/>Antartida</p>
+        <p> <input type="radio" name="continente" value="Oceania"/>Oceania</p>
+
+        
+
+
 
         <input type="submit" value="Enviar">
     </form>
